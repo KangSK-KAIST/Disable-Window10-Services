0d5193ee8a6dac1f4f0cc8689c05bce94dfe9d21
KangSeungKwan <t8235512001@gmail.com>
2020-11-17 02:31:58 +0900

    Made main structure

--- main.bat
+++ main.bat
@@ -4,25 +4,36 @@ REM Written by KangSeungkwan - 20201117
 REM This batch diables services in Windows10.
 
 ECHO -------------------------------------------
-ECHO |       Windows10 Service Minimizer       |
-ECHO |              Made by KangSK             |
+ECHO         Windows10 Service Minimizer        
+ECHO                Made by KangSK              
 ECHO -------------------------------------------
 ECHO/
 ECHO Disabling services might break your system.
 ECHO    To ensure safety, select Manual mode.   
 ECHO/
 
-
 REM Start of main program
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-set SERVICE_NAME="AGMService" "AGSService" "AdobeUpdateService"
-set SERVICE_DISP="Adobe Genuine Moniter Service" "Adobe Genuine Software Integrity Service" "Adobe Update Service"
-
 REM Choose whether disable or just prevent auto-runs
 CHOICE /C DM /N /M "Will you (D)isable or change to (M)anual mode?"
-IF	ERRORLEVEL 1 SET MODE=0:
-IF	ERRORLEVEL 2 SET MODE=1:
+IF ERRORLEVEL 1 SET /A MODE=0:
+IF ERRORLEVEL 2 SET /A MODE=1:
+
+FOR /F "tokens=1,2 delims=," %%i in (database.txt) do (
+	ECHO Shutting Down %%j...
+	sc stop %%i
+	IF %MODE%==0 (
+		REM Disable Service
+		ECHO Disabling Service: %%j...
+		sc config %%i start=disabled
+	) else (
+		REM Change Service to Manual
+		ECHO Disabling Auto-Start: %%j...
+		sc config %%i start=demand
+	)
+	
+)
 
 
 REM End of program
